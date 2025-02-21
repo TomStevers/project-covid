00915602510a358b9d1ff8c93cb7cc4e4c09bb6a
pvandrooge <p.van.drooge@student.vu.nl>
2025-02-21 14:01:45 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import datetime as dt
 
-def createFigure(df):
+def create_figure(df):
     plt.figure(figsize=(12, 10))
     plt.subplot(3,1,1)
     plt.title('Deaths')
@@ -14,7 +14,6 @@ def createFigure(df):
     plt.subplot(3, 1, 3)
     plt.title('New cases')
     plt.plot(df['New cases'])
-    plt.show()
 
 def get_data_frame():
     path = '../../Downloads/day_wise.csv'
@@ -24,15 +23,78 @@ def get_data_frame():
 
     return df
 
-def main():
+def get_data_frame_sir():
+    path = '../../Downloads/day_wise.csv'
+    df = pd.read_csv(path)
+    df = df[['Date', 'Deaths', 'Recovered', 'Active', 'New cases']]
+    df['Date'] = pd.to_datetime(df['Date'])
+
+    return df
+
+def main1():
     df = get_data_frame()
-    createFigure(df)
+    create_figure(df)
     figure_with_dates(df, ['2020-01-22'])
 
 def figure_with_dates(df, data):
     data = pd.to_datetime(data)
     df2 = df[df['Date'].isin(data)]
-    createFigure(df2)
+    create_figure(df2)
 
-main()
+def create_figure_sir(S, I, R, D):
+    plt.figure(figsize=(12,6))
+    plt.plot(S, label = 'Susceptible', color = 'blue')
+    plt.plot(I, label = 'Infected', color = 'red')
+    plt.plot(R, label = 'Recovered', color = 'green')
+    plt.plot(D, label = 'Deceased', color = 'black')
+    plt.xlabel('Days')
+    plt.ylabel('Population')
+    plt.legend()
+    plt.title('Estimated $R_0$ Over Time')
+
+def load_sir():
+    df = get_data_frame_sir()
+    S = []
+    I = []
+    R = []
+    D = []
+
+    # Parameters
+    beta = 0.3  # infection rate
+    gamma = 0.1  # recovery rate
+    mu = 0.05  # mortality rate
+    alpha = 0.0005 # 
+    S0 = 17000000  # initial susceptible population
+    I0 = df['Active'].iloc[0]   # initial infected population
+    R0 = df['Recovered'].iloc[0]    # initial recovered population
+    D0 = df['Deaths'].iloc[0]   # initial deceased population
+    N = S0 + I0 + R0 + D0  # total population
+
+    # Saving first entries to list
+    S.append(S0)
+    I.append(I0)
+    R.append(R0)
+    D.append(D0)
+
+    # Solving equations
+    i = 0
+    for index in df.iterrows():
+        delta_S = alpha*R[i] - beta*S[i]*I[i]/N
+        S.append(S[i] + delta_S)
+        delta_I = beta*S[i]*I[i]/N - mu*I[i] - gamma*I[i]
+        I.append(I[i] + delta_I)
+        delta_R = gamma*I[i] - alpha*R[i]
+        R.append(R[i] + delta_R)
+        delta_D = mu*I[i]
+        D.append(D[i] + delta_D)
+        i += 1
+
+    # Plot results
+    create_figure_sir(S,I,R,D)
+
+main1()
+load_sir() 
+plt.show()
+
+
 
