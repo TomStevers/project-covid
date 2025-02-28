5f41811302466093e852613a57b75105163f86fa
TomStevers <t.d.c.stevers@student.vu.nl>
2025-02-28 23:42:58 +0100

    Part 4 Adjustments

--- part4.py
+++ part4.py
@@ -14,17 +14,17 @@ after_count = df_cleaned.shape[0]
 # Replace missing values with 0
 df_cleaned = df_cleaned.fillna(0)
 
-# Save the cleaned DataFrame back to CSV
+# Save the cleaned DataFrame to new CSV
 df_cleaned.to_csv("cleaned_complete.csv", index=False)
 
-# Look for missing values (after replacement)
+# Look for missing values 
 missing_values = df_cleaned.isnull().sum()
 
 # Print
 print(f"Total Rows Before : {before_count}")
 print(f"Total Rows After: {after_count}")
 print(f"Duplicates removed: {before_count - after_count}")
-print("Missing values per column (after replacement):\n", missing_values)
+print("Missing values per column:\n", missing_values)
 
 
 
