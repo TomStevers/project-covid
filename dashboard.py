5926e99f4a9f9b393072e94799037a6d3769f386
TomStevers <t.d.c.stevers@student.vu.nl>
2025-03-07 14:43:45 +0100

    Create Dashboard

---
+++ dashboard.py
@@ -0,0 +1,11 @@
+import sqlite3
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="Covid Project",
+    layout="wide",
+    initial_sidebar_state="expanded")
