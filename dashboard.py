29ca3995c5012390213aadb07ab949adac3f876d
TomStevers <t.d.c.stevers@student.vu.nl>
2025-03-07 15:02:40 +0100

    Test Dashboard

--- dashboard.py
+++ dashboard.py
@@ -3,9 +3,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
 import streamlit as st
-
+import part3
 
 st.set_page_config(
     page_title="Covid Project",
     layout="wide",
     initial_sidebar_state="expanded")
+
+with st.sidebar:
+    st.title('Sidebar')
+
+st.pyplot(part3.compare_death_rates())
+plt.show
+    
