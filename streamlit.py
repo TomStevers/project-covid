ac897b0829e956fff0d18d58fe0d06ae3704fa4c
SybrandHoekstra <sybrand.hoekstra@gmail.com>
2025-03-19 15:04:05 +0100

    Added the SIRD model to the dashboard

--- streamlit.py
+++ streamlit.py
@@ -65,7 +65,31 @@ with tab1:
 
 with tab2:
     st.title("📈 SIRD Model")
-    st.write("Content for the SIRD Model will be added here.")
+#   st.write("Content for the SIRD Model will be added here.")
+    available_countries = creating_available_countries()
+
+    selected_country = st.sidebar.selectbox("Select a Country", available_countries)
+    r0_data = estimate_parameters(selected_country)    
+    fig = plot_R0_trajectory(r0_data, selected_country)
+    col1, col2 = st.columns([1, 1])
+    
+ # Plot and display the R0 trajectory
+    with col1:
+        # st.subheader(f"R_0 Over Time")
+        fig_r0 = plot_R0_trajectory(r0_data, selected_country)
+    if fig_r0:
+        st.pyplot(fig_r0)
+    else:
+        st.error(f"No R₀ data available for {selected_country}.")
+ 
+ # Plot and display the death rate trajectory
+    with col2:
+     # st.subheader(f"Death Rate Over Time")
+        fig_death = plot_death_rate(r0_data, selected_country)
+        if fig_death:
+            st.pyplot(fig_death)
+        else:
+            st.error(f"No death rate data available for {selected_country}.")
 
 with tab3:
     st.title("🇺🇸 USA Stats")
