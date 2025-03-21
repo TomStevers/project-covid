3c309c46cf47164a939e075d950d525a47542c49
pvandrooge <p.van.drooge@student.vu.nl>
2025-03-21 15:23:21 +0100

    Added China and CAR to database

--- database_extended.py
+++ database_extended.py
@@ -4,48 +4,9 @@ import numpy as np
 import plotly.express as px
 import matplotlib.pyplot as plt
 
-# Establish a persistent database connection
-db_path = "covid_database.db"
-connection = sqlite3.connect(db_path)
-
-def plot_continent_map(continent):
-    if continent == "All":
-        query = """
-            SELECT "Country.Region", ActiveCases, Population
-            FROM worldometer_data
-            WHERE Continent IS NOT NULL
-        """
-        df_continent_map = pd.read_sql(query, connection)
-    else:
-        query = """
-            SELECT "Country.Region", ActiveCases, Population
-            FROM worldometer_data
-            WHERE continent = ?
-        """
-        df_continent_map = pd.read_sql(query, connection, params=(continent,))
-    
-    df_continent_map["Total Cases"] = df_continent_map["ActiveCases"] 
-    # / df_continent_map["Population"]
-    
-    fig = px.choropleth(
-        df_continent_map, 
-        locations="Country.Region", 
-        locationmode="country names", 
-        color="Total Cases", 
-        # title=f"{continent if continent != 'All' else 'All Continents'} Countries with Active Cases", 
-        color_continuous_scale="Turbo"
-    )
-
-    return df_continent_map
-
-# df = plot_continent_map("All").sort_values(by = ['Population'], ascending = False)
-# df['Cases Fraction'] = df['Total Cases']/df['Population']
-# with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-#     print(df)
-
 def update_database():
     # List of all countries that need to be updated
-    countries = ['UK', 'Spain', 'Netherlands', 'Sweden']
+    countries = ['UK', 'Spain', 'Netherlands', 'Sweden', 'China', 'CAR']
     
     # List of new active cases, based on the active cases of a similar country
     cases_fractions = [86204.0, 86204.0, 64503.0, 661.0]
@@ -68,29 +29,52 @@ def update_database():
     # the required country
 
     for country in countries:
+        # France was used for the calculations of the active cases and total recovered for the UK
         if country == 'UK':
             update_query = f"""
             UPDATE worldometer_data
             SET ActiveCases = 86204.0, TotalRecovered = 129880.0
             WHERE "Country.Region" = 'UK'
             """
+        # France was used for the calculations of the active cases and total recovered for Spain
         elif country == 'Spain':
             update_query = f"""
             UPDATE worldometer_data
             SET ActiveCases = 59341.0, TotalRecovered = 149436.0
             WHERE "Country.Region" = 'Spain'
             """
+        # Belgium was used for the calculations of the active cases and total recovered for the Netherlands
         elif country == 'Netherlands':
             update_query = f"""
             UPDATE worldometer_data
             SET ActiveCases = 64503.0, TotalRecovered = 14143.0
             WHERE "Country.Region" = 'Netherlands'
             """
-        else:
+        # Norway was used for the calculations of the active cases and total recovered for Sweden
+        elif country == 'Sweden':
             update_query = f"""
             UPDATE worldometer_data
             SET ActiveCases = 661.0, TotalRecovered = 76677.0
             WHERE "Country.Region" = 'Sweden'
+            """
+        # India was used for the calculations of the total cases, active cases, total deaths and total recovered for China
+        # The data for Population was the approximate population of China in 2020
+        elif countries == 'China' :
+            # Checking if China already exists so if the function is runs twice there won't be a duplicate row 
+            cursor.execute("SELECT COUNT(*) FROM worldometer_data WHERE \"Country.Region\" = 'China'")
+            exists = cursor.fetchone()[0]
+
+            if exists == 0:
+                update_query = f""" 
+                INSERT INTO worldometer_data ('Country.Region', Continent, Population, TotalCases, TotalDeaths, TotalRecovered, ActiveCases, 'WHO.Region')
+                VALUES ("China", "Asia", 1411000000.0, 2068891.0, 42532.0, 1406954.0, 619405.0, "WesternPacific")
+                """
+        # Changing the name of CAR to Central African Republic
+        else :
+            update_query = f"""
+            UPDATE worldometer_data
+            SET "Country.Region" = 'Central African Republic'
+            WHERE "Country.Region" = 'CAR'
             """
 
         cursor.execute(update_query)
@@ -98,5 +82,6 @@ def update_database():
 
     connection.close()
 
+update_database()
 
 
